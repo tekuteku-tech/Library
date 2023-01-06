97a335f4a1f33c93c25800f8be654dd8a004b8f6
tekuteku-tech <tekuteku.tech.mail@gmail.com>
2023-01-06 20:27:26 +0900

    フォーマットを実行

--- src/main/java/com/example/demo/library/LibraryApplication.java
+++ src/main/java/com/example/demo/library/LibraryApplication.java
@@ -1,13 +1,12 @@
 package com.example.demo.library;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-	}
+    }
 
 }
